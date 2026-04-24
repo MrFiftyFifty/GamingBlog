d4314bc76fba56de631b6b0896afdc6ff47c59e1
SoulVayne <gigalo1403@yandex.ru>
2026-04-24 23:46:10 +0300

    notification + email update

--- config/settings.py
+++ config/settings.py
@@ -137,6 +137,15 @@ SOCIALACCOUNT_PROVIDERS = {
     }
 }
 
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'negativpozitiv12@gmail.com'
+EMAIL_HOST_PASSWORD = 'wkauzoncplvoinjz'
+
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
